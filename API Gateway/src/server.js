dbc11d7e50767cac2b84673cf3894cee1c1408e1
Sebastian Ovalle <sebastian.ovalle17@tau.usbmed.edu.co>
2021-12-12 04:31:44 -0500

    Actualizacion Api

--- API Gateway/src/server.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    auth_api_url: 'https://ciclo4dw.herokuapp.com/',
-    request_api_url: 'https://account-ms-sebas231.herokuapp.com/',
-};
--- API Gateway/src/typeDefs/index.js
+++
@@ -1,9 +0,0 @@
-//Se llama al typedef (esquema) de cada submodulo
-const requestTypeDefs = require('./Request_type_defs');
-const authTypeDefs = require('./auth_type_defs');
-
-//Se unen
-const schemasArrays = [authTypeDefs, requestTypeDefs];
-
-//Se exportan
-module.exports = schemasArrays;
